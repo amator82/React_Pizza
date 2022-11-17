f8f64433d73fecce843c753d7cee0962cd2f9e01
Ilya <ilyakorckishko@gmail.com>
2022-11-17 12:05:17 +0200

    focus on search after deletion

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,19 +1,38 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef } from 'react'
 import { SearchContext } from '../../App'
 
 import styles from './Search.module.scss'
 
+import cross from '../../assets/img/cross.svg'
+
 const Search = () => {
     const { searchValue, setSearchValue } = useContext(SearchContext)
+    const inputRef = useRef()
+
+    const onClickCross = () => {
+        setSearchValue('')
+        inputRef.current.focus()
+    }
 
     return (
-        <input
-            value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
-            type='text'
-            placeholder='Поиск пиццы...'
-            className={styles.root}
-        />
+        <div className={styles.wrapper}>
+            <input
+                ref={inputRef}
+                value={searchValue}
+                onChange={(e) => setSearchValue(e.target.value)}
+                type='text'
+                placeholder='Поиск пиццы...'
+                className={styles.root}
+            />
+            {searchValue && (
+                <img
+                    className={styles.cross}
+                    src={cross}
+                    alt='cross'
+                    onClick={onClickCross}
+                />
+            )}
+        </div>
     )
 }
 
