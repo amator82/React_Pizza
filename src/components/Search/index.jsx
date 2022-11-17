7e5071d2dcf021d36279d2ae22af5acd370bafb3
Ilya <ilyakorckishko@gmail.com>
2022-11-17 19:42:59 +0200

    use lodash for component search

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,4 +1,5 @@
-import React, { useContext, useRef } from 'react'
+import React, { useCallback, useContext, useRef, useState } from 'react'
+import debounce from 'lodash.debounce'
 import { SearchContext } from '../../App'
 
 import styles from './Search.module.scss'
@@ -6,25 +7,39 @@ import styles from './Search.module.scss'
 import cross from '../../assets/img/cross.svg'
 
 const Search = () => {
-    const { searchValue, setSearchValue } = useContext(SearchContext)
+    const [value, setValue] = useState('')
+    const { setSearchValue } = useContext(SearchContext)
     const inputRef = useRef()
 
     const onClickCross = () => {
         setSearchValue('')
+        setValue('')
         inputRef.current.focus()
+    }
+
+    const updateSearchValue = useCallback(
+        debounce((str) => {
+            setSearchValue(str)
+        }, 350),
+        []
+    )
+
+    const onChangeInput = (event) => {
+        setValue(event.target.value)
+        updateSearchValue(event.target.value)
     }
 
     return (
         <div className={styles.wrapper}>
             <input
                 ref={inputRef}
-                value={searchValue}
-                onChange={(e) => setSearchValue(e.target.value)}
+                value={value}
+                onChange={onChangeInput}
                 type='text'
                 placeholder='Поиск пиццы...'
                 className={styles.root}
             />
-            {searchValue && (
+            {value && (
                 <img
                     className={styles.cross}
                     src={cross}
