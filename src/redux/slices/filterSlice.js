0dd4be1e9cfacb3e9ebc86f95094c19436cff299
Ilya <ilyakorckishko@gmail.com>
2022-11-21 11:32:23 +0200

    Update filterSlice.js

--- src/redux/slices/filterSlice.js
+++ src/redux/slices/filterSlice.js
@@ -24,9 +24,18 @@ export const filterSlice = createSlice({
             state.currentPage = action.payload
         },
         setFilters(state, action) {
-            state.sort = action.payload.sort
-            state.currentPage = Number(action.payload.currentPage)
-            state.categoryId = Number(action.payload.categoryId)
+            if (Object.keys(action.payload).length) {
+                state.currentPage = Number(action.payload.currentPage)
+                state.categoryId = Number(action.payload.categoryId)
+                state.sort = action.payload.sort
+            } else {
+                state.currentPage = 1
+                state.categoryId = 0
+                state.sort = {
+                    name: 'популярности',
+                    sortProperty: 'rating'
+                }
+            }
         }
     }
 })
