6c410a79e8ae51dcc998883a6331bfea30f5d020
Ilya <ilyakorckishko@gmail.com>
2022-11-28 14:32:32 +0200

    typing onClick, onChange and Event

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -5,7 +5,7 @@ import styles from './Paginatoin.module.scss'
 
 type PaginationProps = {
     currentPage: number
-    onChangePage: any
+    onChangePage: (page: number) => void
 }
 
 const Pagination: React.FC<PaginationProps> = ({
--- src/components/Sort.tsx
+++ src/components/Sort.tsx
@@ -29,8 +29,11 @@ const Sort = () => {
     }
 
     useEffect(() => {
-        const handleClickOutside = (event: any) => {
-            if (!event.path.includes(sortRef.current)) {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (
+                sortRef.current &&
+                !event.composedPath().includes(sortRef.current)
+            ) {
                 setIsOpenPopup(false)
             }
         }
